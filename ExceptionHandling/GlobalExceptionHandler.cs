761a156a007fc540cbed24f62c96037c659dab0e
Mohamed Imam <moeimam27@gmail.com>
2026-08-18 18:32:13 -0700

    feat: add global exception handler

---
+++ ExceptionHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace CarRental.API.ExceptionHandling
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        public GlobalExceptionHandler(
+          ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(
+          HttpContext httpContext,
+          Exception exception,
+          CancellationToken cancellationToken)
+        {
+            // Log the unexpected exception
+            _logger.LogError(
+                exception,
+                "An unexpected error occurred.");
+
+            // Set HTTP status code
+            httpContext.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            // Return safe response to the client
+            await httpContext.Response.WriteAsJsonAsync(
+                new
+                {
+                    statusCode = StatusCodes.Status500InternalServerError,
+                    message = "An unexpected error occurred."
+                },
+                cancellationToken);
+
+            return true;
+        }
+    }
+}
